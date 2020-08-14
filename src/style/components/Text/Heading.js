0ed43a888193144e1b82a23f9a4aca9ea28086bf
DDyakova <petar.dyakov@icloud.com>
2020-08-14 09:26:19 +0300

    Updated heading styles and added underline option

--- src/style/components/Text/Heading.js
+++ src/style/components/Text/Heading.js
@@ -2,23 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import colors from "../../colors";
+import Spacing from "../Spacing";
 
 const HeaderTag = styled.h1`
   ${props => {
     let settings = `
-    font-family: ${!!props.font ? `${props.font}` : 'Playfair Display'};
-    font-weight: ${!!props.weight ? `${props.weight}` : 'bold'};
+    font-family: ${!!props.font ? `${props.font}` : "Playfair Display"};
+    font-weight: ${!!props.weight ? `${props.weight}` : "bold"};
     color:  ${!!props.color ? colors[props.color] : colors.white};
   `;
     switch (props.size) {
       case "s":
-        return settings.concat(`font-size: 20px;`);
+        return settings.concat(`
+          font-size: 20px;
+          line-height: 23px;
+        `);
       case "m":
-        return settings.concat(`font-size: 28px;`);
+        return settings.concat(`
+          font-size: 28px;
+          line-height: 37px;
+        `);
       case "l":
-        return settings.concat(`font-size: 40px;`);
+        return settings.concat(`
+          font-size: 40px;
+          line-height: 53px;
+        `);
       case "xl":
-        return settings.concat(`font-size: 48px;`);
+        return settings.concat(`
+          font-size: 48px;
+          line-height: 64px;
+        `);
       default:
         console.warn("Forgot to specify the size of the heading");
         return settings.concat(`color: #FF0000;`);
@@ -26,11 +39,21 @@ const HeaderTag = styled.h1`
   }}
 `;
 
+const Underline = styled.div`
+  height: 1px;
+  width: 141px;
+  background: ${colors.taupe};
+`;
+
 const Heading = props => {
   return (
-    <HeaderTag size={props.size} color={props.color} {...props}>
-      {props.children}
-    </HeaderTag>
+    <>
+      <HeaderTag size={props.size} color={props.color} {...props}>
+        {props.children}
+      </HeaderTag>
+      <Spacing height="19px" />
+      {props.underline && <Underline />}
+    </>
   );
 };
 
@@ -39,7 +62,8 @@ Heading.propTypes = {
   color: PropTypes.string,
   className: PropTypes.string,
   font: PropTypes.string,
-  weight: PropTypes.string
+  weight: PropTypes.string,
+  underline: PropTypes.bool
 };
 
 export default Heading;
