469f1b8c037d25491090783e674dc37db3893e7f
PepoDyakov <petar.dyakov@icloud.com>
2020-08-24 18:03:25 +0300

    adding grid to recipePost.js

--- src/templates/recipePost.js
+++ src/templates/recipePost.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+import colors from "../style/colors";
 import GlobalStyle from "../style/global/GlobalStyle";
 import Heading from "../style/components/Text/Heading";
 import Spacing from "../style/components/Spacing";
@@ -31,13 +32,40 @@ const Inner = styled.div`
 `;
 
 const StepList = styled.ol`
+  list-style: none;
   list-style-position: inside;
   font-family: Inter;
   font-size: 16px;
   font-weight: regular;
+  counter-reset: list-counter;
 `;
 
-const Step = styled.li``;
+const Step = styled.li`
+  counter-increment: list-counter;
+
+  &:before {
+    content: counter(list-counter);
+    font-family: Playfair Display;
+    color: ${colors.taupe};
+    font-size: 24px;
+    margin-right: 16px;
+  }
+
+  &:not(:last-child) {
+    margin-bottom: 18px;
+  }
+`;
+
+const GridSection = styled.div`
+  display: grid;
+  grid-template-columns: repeat(8, 118px [col-start]);
+  grid-template-rows: repeat(16, auto [row-start]);
+`;
+
+const GridElement = styled.div`
+  grid-column: span ${props => !!props.columns && props.columns};
+  grid-row: span ${props => !!props.rows && props.rows};
+`;
 
 const Template = ({ data, pageContext }) => {
   return (
@@ -47,18 +75,20 @@ const Template = ({ data, pageContext }) => {
       <Layout>
         <Hero />
         <Section>
-          <Vertical>
-            <Heading size="l" color="taupe">
-              Recipe overview
-            </Heading>
-            <Spacing height="16px" />
-            <Inner>
-              <Inner vertical width="66%">
-                <Text type="body" color="taupe" align="left">
-                  "We find that extraction is higher (19-22% EXT) without over extracting. Like most Aeropress cups, this is full bodied. We do not dilute. We have been able to create incredibly complex and vibrant cups with most coffees we have brewed using this method.”
-                </Text>
-              </Inner>
+          <GridSection>
+            <GridElement columns="4" rows="1">
+              <Heading size="l" color="taupe">
+                Recipe overview
+              </Heading>
+              <Spacing height="16px" />
+            </GridElement>
+            <GridElement columns="5" rows="1">
+              <Text type="body" color="taupe" align="left">
+                "We find that extraction is higher (19-22% EXT) without over extracting. Like most Aeropress cups, this is full bodied. We do not dilute. We have been able to create incredibly complex and vibrant cups with most coffees we have brewed using this method.”
+              </Text>
               <Spacing width="16px" />
+            </GridElement>
+            <GridElement columns="3" rows="1">
               <Vertical>
                 <Inner width="100%">
                     <Heading size="s" color="taupe">Creator</Heading>
@@ -70,18 +100,36 @@ const Template = ({ data, pageContext }) => {
                     <Text type="body" color="taupe">Chucky's Sofia</Text>
                 </Inner>
               </Vertical>
-            </Inner>
-            <Spacing height="53px" />
-            <Inner>
+            </GridElement>
+            <GridElement columns="5" rows="8">
+              <Spacing height="53px" />
               <Vertical>
                 <Heading size="l" color="taupe">Method</Heading>
                 <Spacing height="24px" />
                 <StepList>
                   <Step>Make sure you completely soak the paper filter with hot water. Use a lot of water ;)</Step>
+                  <Step>Make sure you completely soak the paper filter with hot water. Use a lot of water ;)</Step>
+                  <Step>Make sure you completely soak the paper filter with hot water. Use a lot of water ;)</Step>
+                  <Step>Make sure you completely soak the paper filter with hot water. Use a lot of water ;)</Step>
+                  <Step>Make sure you completely soak the paper filter with hot water. Use a lot of water ;)</Step>
                 </StepList>
               </Vertical>
-            </Inner>
-          </Vertical>
+            </GridElement>
+            <GridElement columns="3" rows="1">
+              <Spacing height="53px" />
+                <Vertical>
+                  <Inner width="100%">
+                      <Heading size="s" color="taupe">Creator</Heading>
+                      <Text type="body" color="taupe">Chucky's Sofia</Text>
+                  </Inner>
+                  <Spacing height="24px" />
+                  <Inner width="100%">
+                      <Heading size="s" color="taupe">Brew type</Heading>
+                      <Text type="body" color="taupe">Chucky's Sofia</Text>
+                  </Inner>
+                </Vertical>
+            </GridElement>
+          </GridSection>
         </Section>
         <Section small>
           <Heading size="l" color="taupe">
